763084b061332e757ea6f0ed719efde68b7126c4
lorenzorizzolo <lolloriz36@gmail.com>
2023-11-23 10:42:27 +0100

    Aggiunto

--- src/mainpage/page.php
+++ src/mainpage/page.php
@@ -1,17 +1,7 @@
 <?php
 
-if(!isset($_SESSION['role'])){
-    $page = isset($_GET['page']) ? $_GET['page'] : "login";
-    require_once $pages.$page.".php";
-}else{
-    $panel_pages =  $_SESSION['role']=="admin" ? $GLOBALS['admin_pages'] : $GLOBALS['user_pages'];
-    $page = isset($_GET['page']) ? $_GET['page'] : "home";
-    if($page=="logout"){ redirect("?logout"); }
-    $final_page = $_SESSION['role']=="admin" ? $src."adminpages/index.php" : $src."userpages/index.php";
-    if(file_exists($final_page)){ require_once $final_page; }
-}
-
 
 
 ?>
 
+qui gestisci il routing delle pagine
